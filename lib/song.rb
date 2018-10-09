5de4cf5ed04948906da27a82ef9ecc53bbd784ce
Tim Knoll <timothy.knoll@gmail.com>
2018-10-09 16:10:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,10 @@ class Song
 
   def self.create_by_filename(filename)
     result = self.new_from_filename(filename)
-    song.save
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
   end
 
   def self.destroy_all
